a390f36ebefe7706a92baf261e076e5694c3bffe
clearblueskypanorama-dev <devcbsp@proton.me>
2024-07-10 10:36:32 +0000

    edit: building

--- src/index.ts
+++ src/index.ts
@@ -91,3 +91,12 @@ export class EventTarget2 extends EventTarget {
         this._bubbleMap.delete(type)
     }
 }
+
+export async function atomicDo(func: () => PromiseLike<any> | any, target: EventTarget2, state: number | string | symbol = "busy", endEvent = "end") {
+    if (target.state === state) await target.waitFor(endEvent);
+    const previousState = target.state
+    target.state = state
+    const result = await func()
+    target.state = previousState
+    target.dispatch(endEvent)
+}
