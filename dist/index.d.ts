c8f2cc22ce27c9c78f332a584762647a77bee8f6
clearblueskypanorama-dev <devcbsp@proton.me>
2024-07-11 10:18:13 +0000

    edit: publish to github

---
+++ dist/index.d.ts
@@ -0,0 +1,28 @@
+type EventListener2<T = any, R = any> = (evt: CustomEvent<T>) => R;
+type EventTarget2State = number | string | symbol;
+export declare class EventTarget2 extends EventTarget {
+    parent?: EventTarget2;
+    state?: EventTarget2State;
+    waitFor(type: string): Promise<unknown>;
+    callback<T>(type: string, callback: (result?: T) => void): void;
+    dispatch<T>(type: string, detail?: T): void;
+    listen<T, R>(type: string, callback: EventListener2<T, R>, options?: boolean | AddEventListenerOptions | undefined): void;
+    remove<T, R>(type: string, callback: EventListener2<T, R>, options?: boolean | AddEventListenerOptions | undefined): void;
+    listenOnce<T, R>(type: string, callback: EventListener2<T, R>): void;
+    listenOnceOnly<T, R>(type: string, callback: EventListener2<T, R>, only: EventListener2<T, boolean>): void;
+    listenDebounce<T, R>(type: string, callback: EventListener2<T, R>, options?: {
+        timeout: number;
+        mode: "first" | "last";
+    } & AddEventListenerOptions): void;
+    listenDebounceFirst<T, R>(type: string, callback: EventListener2<T, R>, options?: {
+        timeout: number;
+    } & AddEventListenerOptions): void;
+    listenDebounceLast<T, R>(type: string, callback: EventListener2<T, R>, options?: {
+        timeout: number;
+    } & AddEventListenerOptions): void;
+    protected _bubbleMap: Map<string, EventListener2>;
+    enableBubble(type: string): void;
+    disableBubble(type: string): void;
+}
+export declare function atomicDo(func: () => PromiseLike<any> | any, target: EventTarget2, state?: number | string | symbol, endEvent?: string): Promise<void>;
+export {};
---
+++ dist/index.js
@@ -0,0 +1,94 @@
+"use strict";
+(() => {
+  // src/index.ts
+  var EventTarget2 = class extends EventTarget {
+    constructor() {
+      super(...arguments);
+      this._bubbleMap = /* @__PURE__ */ new Map();
+    }
+    async waitFor(type) {
+      return new Promise((resolve) => {
+        this.addEventListener(type, resolve, { once: true });
+      });
+    }
+    callback(type, callback) {
+      this.waitFor(type).then((result) => callback(result));
+    }
+    dispatch(type, detail) {
+      this.dispatchEvent(new CustomEvent(type, detail ? { detail } : void 0));
+    }
+    listen(type, callback, options) {
+      this.addEventListener(type, callback, options);
+    }
+    remove(type, callback, options) {
+      this.removeEventListener(type, callback, options);
+    }
+    listenOnce(type, callback) {
+      this.listen(type, callback, { once: true });
+    }
+    listenOnceOnly(type, callback, only) {
+      const wrapper = (e) => {
+        if (only(e)) {
+          this.remove(type, wrapper);
+          callback(e);
+        }
+      };
+      this.listen(type, wrapper);
+    }
+    listenDebounce(type, callback, options = { timeout: 100, mode: "last" }) {
+      switch (options.mode) {
+        case "first":
+          return this.listenDebounceFirst(type, callback, options);
+        case "last":
+          return this.listenDebounceLast(type, callback, options);
+      }
+    }
+    listenDebounceFirst(type, callback, options = { timeout: 100 }) {
+      let lastMs = 0;
+      this.listen(
+        type,
+        (e) => {
+          const currentMs = Date.now();
+          if (currentMs - lastMs > options.timeout) {
+            callback(e);
+          }
+          lastMs = currentMs;
+        },
+        options
+      );
+    }
+    listenDebounceLast(type, callback, options = { timeout: 100 }) {
+      let timoutInstance;
+      this.listen(
+        type,
+        (e) => {
+          clearTimeout(timoutInstance);
+          timoutInstance = window.setTimeout(() => callback(e), options.timeout);
+        },
+        options
+      );
+    }
+    enableBubble(type) {
+      if (this._bubbleMap.has(type)) return;
+      const dispatcher = (e) => {
+        this.parent?.dispatch(e.type, e.detail);
+      };
+      this.listen(type, dispatcher);
+      this._bubbleMap.set(type, dispatcher);
+    }
+    disableBubble(type) {
+      if (!this._bubbleMap.has(type)) return;
+      const dispatcher = this._bubbleMap.get(type);
+      this.remove(type, dispatcher);
+      this._bubbleMap.delete(type);
+    }
+  };
+  async function atomicDo(func, target, state = "busy", endEvent = "end") {
+    if (target.state === state) await target.waitFor(endEvent);
+    const previousState = target.state;
+    target.state = state;
+    const result = await func();
+    target.state = previousState;
+    target.dispatch(endEvent);
+  }
+})();
