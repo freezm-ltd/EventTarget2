0a81e334e73a9501b94c6d78441008a32c6f4bfe
DESKTOP-4ON57L8\Admin <devcbsp@proton.me>
2024-07-26 17:26:46 +0900

    add: test atomic

---
+++ test/index.js
@@ -0,0 +1,126 @@
+// src/index.ts
+var EventTarget2 = class extends EventTarget {
+  constructor() {
+    super(...arguments);
+    this.listeners = /* @__PURE__ */ new Map();
+    this._bubbleMap = /* @__PURE__ */ new Map();
+    this.atomicQueue = /* @__PURE__ */ new Map();
+  }
+  async waitFor(type, compareValue) {
+    return new Promise((resolve) => {
+      if (compareValue !== void 0) {
+        this.listenOnceOnly(type, resolve, (e) => e.detail === compareValue);
+      } else {
+        this.listenOnce(type, resolve);
+      }
+    });
+  }
+  callback(type, callback) {
+    this.waitFor(type).then((result) => callback(result));
+  }
+  dispatch(type, detail) {
+    this.dispatchEvent(new CustomEvent(type, detail !== void 0 ? { detail } : void 0));
+  }
+  listen(type, callback, options) {
+    if (!this.listeners.has(type)) this.listeners.set(type, /* @__PURE__ */ new Set());
+    this.listeners.get(type).add(callback);
+    this.addEventListener(type, callback, options);
+  }
+  remove(type, callback, options) {
+    if (!this.listeners.has(type)) this.listeners.set(type, /* @__PURE__ */ new Set());
+    this.listeners.get(type).delete(callback);
+    this.removeEventListener(type, callback, options);
+  }
+  destroy() {
+    for (let type of this.listeners.keys()) {
+      for (let callback of this.listeners.get(type)) {
+        this.remove(type, callback);
+      }
+    }
+  }
+  listenOnce(type, callback) {
+    this.listen(type, callback, { once: true });
+  }
+  listenOnceOnly(type, callback, only) {
+    const wrapper = (e) => {
+      if (only(e)) {
+        this.remove(type, wrapper);
+        callback(e);
+      }
+    };
+    this.listen(type, wrapper);
+  }
+  listenDebounce(type, callback, options = { timeout: 100, mode: "last" }) {
+    switch (options.mode) {
+      case "first":
+        return this.listenDebounceFirst(type, callback, options);
+      case "last":
+        return this.listenDebounceLast(type, callback, options);
+    }
+  }
+  listenDebounceFirst(type, callback, options = { timeout: 100 }) {
+    let lastMs = 0;
+    this.listen(
+      type,
+      (e) => {
+        const currentMs = Date.now();
+        if (currentMs - lastMs > options.timeout) {
+          callback(e);
+        }
+        lastMs = currentMs;
+      },
+      options
+    );
+  }
+  listenDebounceLast(type, callback, options = { timeout: 100 }) {
+    let timoutInstance;
+    this.listen(
+      type,
+      (e) => {
+        clearTimeout(timoutInstance);
+        timoutInstance = window.setTimeout(() => callback(e), options.timeout);
+      },
+      options
+    );
+  }
+  enableBubble(type) {
+    if (this._bubbleMap.has(type)) return;
+    const dispatcher = (e) => {
+      this.parent?.dispatch(e.type, e.detail);
+    };
+    this.listen(type, dispatcher);
+    this._bubbleMap.set(type, dispatcher);
+  }
+  disableBubble(type) {
+    if (!this._bubbleMap.has(type)) return;
+    const dispatcher = this._bubbleMap.get(type);
+    this.remove(type, dispatcher);
+    this._bubbleMap.delete(type);
+  }
+  _atomicInit(type) {
+    this.atomicQueue.set(type, []);
+    const atomicLoop = async () => {
+      const queue = this.atomicQueue.get(type);
+      while (true) {
+        const task = queue.shift();
+        if (task) {
+          await task();
+        } else {
+          await this.waitFor("__atomic-add", type);
+        }
+      }
+    };
+    atomicLoop();
+  }
+  atomic(type, func) {
+    return new Promise((resolve) => {
+      const wrap = async () => resolve(await func());
+      if (!this.atomicQueue.has(type)) this._atomicInit(type);
+      this.atomicQueue.get(type).push(wrap);
+      this.dispatch("__atomic-add", type);
+    });
+  }
+};
+export {
+  EventTarget2
+};
