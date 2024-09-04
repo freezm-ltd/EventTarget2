630b4b6d1e82586d3ff7ea4d11694eefdc2db051
DESKTOP-4ON57L8\Admin <devcbsp@proton.me>
2024-09-04 20:49:27 +0900

    add: EventMap support

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,33 +1,36 @@
 export type EventListener2<T = any, R = any> = (evt: CustomEvent<T>) => R;
 export type EventTarget2State = number | string | symbol;
-export declare class EventTarget2 extends EventTarget {
+type _EventMapKey<EventMap> = Extract<keyof EventMap, string>;
+type EventMapKey<EventMap> = (_EventMapKey<EventMap> extends never ? string : _EventMapKey<EventMap>);
+export declare class EventTarget2<EventMap extends Record<string, Event> = {}, K extends string = EventMapKey<EventMap>> extends EventTarget {
     parent?: EventTarget2;
     state?: EventTarget2State;
     listeners: Map<string, Set<EventListener2>>;
-    waitFor<T>(type: string, compareValue?: T): Promise<T>;
-    callback<T>(type: string, callback: (result?: T) => void): void;
-    dispatch<T>(type: string, detail?: T): void;
-    listen<T, R = void>(type: string, callback: EventListener2<T, R>, options?: boolean | AddEventListenerOptions | undefined): void;
-    remove<T, R = void>(type: string, callback: EventListener2<T, R>, options?: boolean | AddEventListenerOptions | undefined): void;
+    waitFor<T>(type: K, compareValue?: T): Promise<T>;
+    callback<T>(type: K, callback: (result?: T) => void): void;
+    dispatch<T>(type: K, detail?: T): void;
+    listen<T, R = void>(type: K, callback: EventListener2<T, R>, options?: boolean | AddEventListenerOptions | undefined): void;
+    remove<T, R = void>(type: K, callback: EventListener2<T, R>, options?: boolean | AddEventListenerOptions | undefined): void;
     destroy(): void;
-    listenOnce<T, R = void>(type: string, callback: EventListener2<T, R>): void;
-    listenOnceOnly<T, R = void>(type: string, callback: EventListener2<T, R>, only: EventListener2<T, boolean>): void;
-    listenWhile<T, R = void>(type: string, callback: EventListener2<T, R>, whileFunc: EventListener2<T, boolean>): void;
-    listenDebounce<T, R = void>(type: string, callback: EventListener2<T, R>, options?: {
+    listenOnce<T, R = void>(type: K, callback: EventListener2<T, R>): void;
+    listenOnceOnly<T, R = void>(type: K, callback: EventListener2<T, R>, only: EventListener2<T, boolean>): void;
+    listenWhile<T, R = void>(type: K, callback: EventListener2<T, R>, whileFunc: EventListener2<T, boolean>): void;
+    listenDebounce<T, R = void>(type: K, callback: EventListener2<T, R>, options?: {
         timeout: number;
         mode: "first" | "last";
     } & AddEventListenerOptions): void;
-    listenDebounceFirst<T, R = void>(type: string, callback: EventListener2<T, R>, options?: {
+    listenDebounceFirst<T, R = void>(type: K, callback: EventListener2<T, R>, options?: {
         timeout: number;
     } & AddEventListenerOptions): void;
-    listenDebounceLast<T, R = void>(type: string, callback: EventListener2<T, R>, options?: {
+    listenDebounceLast<T, R = void>(type: K, callback: EventListener2<T, R>, options?: {
         timeout: number;
     } & AddEventListenerOptions): void;
     static race<T, R = void>(targets: Array<EventTarget2>, type: string, callback: EventListener2<T, R>): void;
     protected _bubbleMap: Map<string, EventListener2>;
-    enableBubble(type: string, parentFunc?: () => EventTarget2 | undefined): void;
-    disableBubble(type: string): void;
+    enableBubble(type: K, parentFunc?: () => EventTarget2 | undefined): void;
+    disableBubble(type: K): void;
     protected atomicQueue: Map<string, Array<() => PromiseLike<any> | any>>;
-    _atomicInit(type: string): void;
-    atomic<T>(type: string, func: () => PromiseLike<T> | T): Promise<T>;
+    _atomicInit(type: K): void;
+    atomic<T>(type: K, func: () => PromiseLike<T> | T): Promise<T>;
 }
+export {};
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
 export type EventListener2<T = any, R = any> = (evt: CustomEvent<T>) => R
 export type EventTarget2State = number | string | symbol
 
-export class EventTarget2 extends EventTarget {
+type _EventMapKey<EventMap> = Extract<keyof EventMap, string>
+type EventMapKey<EventMap> = (_EventMapKey<EventMap> extends never ? string : _EventMapKey<EventMap>)
+
+export class EventTarget2<EventMap extends Record<string, Event> = {}, K extends string = EventMapKey<EventMap>> extends EventTarget {
     parent?: EventTarget2;
     state?: EventTarget2State
     listeners: Map<string, Set<EventListener2>> = new Map()
 
-    async waitFor<T>(type: string, compareValue?: T): Promise<T> {
+    async waitFor<T>(type: K, compareValue?: T): Promise<T> {
         return new Promise((resolve) => {
             if (compareValue !== undefined) {
                 this.listenOnceOnly<T, void>(type, (e) => resolve(e.detail), (e) => e.detail === compareValue)
@@ -16,21 +19,21 @@ export class EventTarget2 extends EventTarget {
         });
     }
 
-    callback<T>(type: string, callback: (result?: T) => void) {
+    callback<T>(type: K, callback: (result?: T) => void) {
         this.waitFor<T>(type).then(callback);
     }
 
-    dispatch<T>(type: string, detail?: T) {
+    dispatch<T>(type: K, detail?: T) {
         this.dispatchEvent(new CustomEvent(type, detail !== undefined ? { detail } : undefined));
     }
 
-    listen<T, R = void>(type: string, callback: EventListener2<T, R>, options?: boolean | AddEventListenerOptions | undefined) {
+    listen<T, R = void>(type: K, callback: EventListener2<T, R>, options?: boolean | AddEventListenerOptions | undefined) {
         if (!this.listeners.has(type)) this.listeners.set(type, new Set())
         this.listeners.get(type)!.add(callback)
         this.addEventListener(type, callback as unknown as EventListener, options);
     }
 
-    remove<T, R = void>(type: string, callback: EventListener2<T, R>, options?: boolean | AddEventListenerOptions | undefined) {
+    remove<T, R = void>(type: K, callback: EventListener2<T, R>, options?: boolean | AddEventListenerOptions | undefined) {
         if (!this.listeners.has(type)) this.listeners.set(type, new Set())
         this.listeners.get(type)!.delete(callback)
         this.removeEventListener(type, callback as unknown as EventListener, options);
@@ -39,16 +42,16 @@ export class EventTarget2 extends EventTarget {
     destroy() {
         for (let type of this.listeners.keys()) {
             for (let callback of this.listeners.get(type)!) {
-                this.remove(type, callback)
+                this.remove(type as K, callback)
             }
         }
     }
 
-    listenOnce<T, R = void>(type: string, callback: EventListener2<T, R>) {
+    listenOnce<T, R = void>(type: K, callback: EventListener2<T, R>) {
         this.listen<T, R>(type, callback, { once: true });
     }
 
-    listenOnceOnly<T, R = void>(type: string, callback: EventListener2<T, R>, only: EventListener2<T, boolean>) {
+    listenOnceOnly<T, R = void>(type: K, callback: EventListener2<T, R>, only: EventListener2<T, boolean>) {
         const wrapper = (e: CustomEvent<T>) => {
             if (only(e)) {
                 this.remove(type, wrapper)
@@ -58,7 +61,7 @@ export class EventTarget2 extends EventTarget {
         this.listen<T, void>(type, wrapper)
     }
 
-    listenWhile<T, R = void>(type: string, callback: EventListener2<T, R>, whileFunc: EventListener2<T, boolean>) {
+    listenWhile<T, R = void>(type: K, callback: EventListener2<T, R>, whileFunc: EventListener2<T, boolean>) {
         const wrapper = (e: CustomEvent<T>) => {
             callback(e)
             if (!whileFunc(e)) {
@@ -68,14 +71,14 @@ export class EventTarget2 extends EventTarget {
         this.listen<T, void>(type, wrapper)
     }
 
-    listenDebounce<T, R = void>(type: string, callback: EventListener2<T, R>, options: { timeout: number, mode: "first" | "last" } & AddEventListenerOptions = { timeout: 100, mode: "last" }) {
+    listenDebounce<T, R = void>(type: K, callback: EventListener2<T, R>, options: { timeout: number, mode: "first" | "last" } & AddEventListenerOptions = { timeout: 100, mode: "last" }) {
         switch (options.mode) {
             case "first": return this.listenDebounceFirst<T, R>(type, callback, options);
             case "last": return this.listenDebounceLast<T, R>(type, callback, options);
         }
     }
 
-    listenDebounceFirst<T, R = void>(type: string, callback: EventListener2<T, R>, options: { timeout: number } & AddEventListenerOptions = { timeout: 100 }) {
+    listenDebounceFirst<T, R = void>(type: K, callback: EventListener2<T, R>, options: { timeout: number } & AddEventListenerOptions = { timeout: 100 }) {
         let lastMs = 0
         this.listen<T, void>(
             type,
@@ -90,7 +93,7 @@ export class EventTarget2 extends EventTarget {
         )
     }
 
-    listenDebounceLast<T, R = void>(type: string, callback: EventListener2<T, R>, options: { timeout: number } & AddEventListenerOptions = { timeout: 100 }) {
+    listenDebounceLast<T, R = void>(type: K, callback: EventListener2<T, R>, options: { timeout: number } & AddEventListenerOptions = { timeout: 100 }) {
         let timoutInstance: number;
         this.listen<T, void>(
             type,
@@ -119,7 +122,7 @@ export class EventTarget2 extends EventTarget {
     }
 
     protected _bubbleMap: Map<string, EventListener2> = new Map()
-    enableBubble(type: string, parentFunc?: () => EventTarget2 | undefined) {
+    enableBubble(type: K, parentFunc?: () => EventTarget2 | undefined) {
         if (this._bubbleMap.has(type)) return;
         const dispatcher = (e: CustomEvent) => {
             const parent = parentFunc ? parentFunc() : this.parent
@@ -128,7 +131,7 @@ export class EventTarget2 extends EventTarget {
         this.listen(type, dispatcher);
         this._bubbleMap.set(type, dispatcher)
     }
-    disableBubble(type: string) {
+    disableBubble(type: K) {
         if (!this._bubbleMap.has(type)) return;
         const dispatcher = this._bubbleMap.get(type)!
         this.remove(type, dispatcher)
@@ -136,7 +139,7 @@ export class EventTarget2 extends EventTarget {
     }
 
     protected atomicQueue: Map<string, Array<() => PromiseLike<any> | any>> = new Map()
-    _atomicInit(type: string) {
+    _atomicInit(type: K) {
         this.atomicQueue.set(type, []);
         const atomicLoop = async () => {
             const queue = this.atomicQueue.get(type)!
@@ -145,18 +148,18 @@ export class EventTarget2 extends EventTarget {
                 if (task) {
                     await task()
                 } else {
-                    await this.waitFor("__atomic-add", type);
+                    await this.waitFor("__atomic-add" as any, type);
                 }
             }
         }
         atomicLoop()
     }
-    atomic<T>(type: string, func: () => PromiseLike<T> | T) {
+    atomic<T>(type: K, func: () => PromiseLike<T> | T) {
         return new Promise<T>((resolve) => {
             const wrap = async () => resolve(await func())
             if (!this.atomicQueue.has(type)) this._atomicInit(type);
             this.atomicQueue.get(type)!.push(wrap)
-            this.dispatch("__atomic-add", type)
+            this.dispatch("__atomic-add" as any, type)
         })
     }
 }
